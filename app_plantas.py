99c89bb2ab54647a2d8677e6a3967ef52aba93b3
Bruno Arezo <arezobruno1999@gmail.com>
2025-08-27 21:02:04 -0300

    streamlit_upd

--- app_plantas.py
+++ app_plantas.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 from PIL import Image
 import pandas as pd
 import pickle
-import plotly.express as px
-import plotly.graph_objects as go
 
 try:
     import tensorflow as tf
